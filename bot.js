7da2828970d788a0efa86bd29b6121fe91977c84
EdanDanino <47155956+EdanDanino@users.noreply.github.com>
2019-11-21 19:47:07 +0200

    Finished

--- bot.js
+++ bot.js
@@ -1,9 +1,31 @@
-var Discord = require("discord.io");
-var logger = require("winston");
-var auth = require("./auth.json");
-var events = new Array();
+const Discord = require("discord.io");
+const logger = require("winston");
+const auth = require("./auth.json");
+const fs = require("fs");
+const ct = require("countries-and-timezones");
+
 const infoMsg =
-  "Hello i Am HoonBot. My prefix is : * , My commends are : Info,Events,AddEvent,DisplayTimeZones.  IMPORTENT! ,AddEvents Commend works Only with JsonFiles for Now ! : See Example : '*AddEvents {'Name':'InsertName','Date':'InsertDate'} ";
+  "Hello i Am HoonBot. \n My prefix is : * \n My commends are : Info,Events,AddEvent,DisplayTimeZones ";
+
+const writeEvent = data => {
+  fs.writeFile("./info.json", JSON.stringify(data), err => {
+    if (err) console.log("Error writing file:", err);
+  });
+};
+const createJsonForEvent = (name, date, time, timezone) => {
+  let data = {};
+  data.name = name;
+  data.date = date;
+  data.time = time;
+  const timezoneCt = ct.getTimezone(timezone);
+  data.timezone = timezoneCt;
+  console.log(JSON.stringify(data), "JSONDATA");
+  return data;
+};
+const readJSONFile = () => {
+  return (rawFile = require("./info.json"));
+};
+var file = readJSONFile();
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console(), {
   colorize: true
@@ -26,29 +48,62 @@ bot.on("message", function(user, userID, channelID, message, evt) {
     console.log("Cmd", cmd);
     args = args.splice(1);
     switch (cmd) {
-      case "Info":
+      case "info":
         bot.sendMessage({
           to: channelID,
           message: infoMsg
         });
         break;
-      case "Poke":
+      case "poke":
         bot.sendMessage({
           to: channelID,
           message: "Rawr!"
         });
         break;
-      case "Events":
+      case "event":
+        let msg = `Name : ${file.name},Date : ${file.date},Time : ${file.time},TimeZone : ${file.timezone.utcOffsetStr}`;
         bot.sendMessage({
           to: channelID,
-          message: JSON.stringify(events)
+          message: msg || "There are no Events"
         });
         break;
-      case "AddEvents":
-        events.push(JSON.parse(args[1]));
+      case "addEvent":
+        console.log(args.length);
+        if (args.length > 1 && args[3].includes("/")) {
+          let tempEvent = createJsonForEvent(
+            args[0] || "",
+            args[1] || "",
+            args[2] || "",
+            args[3] || ""
+          );
+          writeEvent(tempEvent);
+          bot.sendMessage({
+            to: channelID,
+            message: "Event Have been added !"
+          });
+        } else {
+          bot.sendMessage({
+            to: channelID,
+            message:
+              "make sure u add name date time and timezone after the commend!"
+          });
+        }
+        break;
+      case "DisplayTimeZones":
+        const timezoneIL = ct.getTimezone("Asia/Tel_Aviv");
+        const timezoneNJ = ct.getTimezone("America/New_York");
+        const timezoneFR = ct.getTimezone("Europe/Paris");
         bot.sendMessage({
           to: channelID,
-          message: "Event Have been added !"
+          message:
+            `Times display in UTC : \n Timezone In Asia/Tel_Aviv ${timezoneIL.utcOffsetStr} \n Timezone In America/New_York ${timezoneNJ.utcOffsetStr} \n Timezone In Europe/Paris ${timezoneFR.utcOffsetStr}`
+        });
+        break;
+        case "ShowExamples":     
+        bot.sendMessage({
+          to: channelID,
+          message:
+            "addEvent commend Example :\n *addEvent Alex's_Birthday 7/7 3Pm America/New_York"
         });
         break;
     }
